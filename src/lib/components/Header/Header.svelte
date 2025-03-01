33ce03081b6e61bd519c58adf0bac2dcc83182a7
Takunda <takunda.muzunze@isitetv.com>
2025-03-01 17:40:37 +0000

    Feat: Create header component.

---
+++ src/lib/components/Header/Header.svelte
@@ -0,0 +1,13 @@
+<header class="flex w-full flex-row items-center justify-between p-5 text-white">
+	<a href="/" class="">
+		<h1>Cinescope</h1>
+	</a>
+
+	<nav aria-label="Main Navigation">
+		<ul role="list" class="flex gap-5">
+			<li><a href="/" class="hover:underline">Home</a></li>
+			<li><a href="/about" class="hover:underline">About</a></li>
+			<li><a href="/faqs" class="hover:underline">FAQs</a></li>
+		</ul>
+	</nav>
+</header>
