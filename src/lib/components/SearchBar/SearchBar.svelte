b563d95bd4ad412aeef1e2b550a7e263d33511b7
Takunda <takunda.muzunze@isitetv.com>
2025-03-01 21:08:18 +0000

    Feat: Create search bar component.

---
+++ src/lib/components/SearchBar/SearchBar.svelte
@@ -0,0 +1,24 @@
+<script lang="ts">
+	import { onMount } from 'svelte';
+	import { fetchMovies } from '../../../routes/backend/fetchMovies';
+
+	let query = $state('');
+	let results = $state([]);
+
+	async function initiateSearch() {
+		results = await fetchMovies(query);
+	}
+
+	onMount(async () => {
+		await initiateSearch();
+	});
+</script>
+
+<div class="w-full">
+	<input
+		class="focus:border-accent w-full rounded-lg border border-gray-300 bg-white/10 p-2 p-3 text-sm focus:outline-none"
+		type="text"
+		placeholder="Search for a movie..."
+		bind:value={query}
+	/>
+</div>
