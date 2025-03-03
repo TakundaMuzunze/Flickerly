ef1762928e930e1e3be0b7578f803d068eed5cc4
Takunda <takunda.muzunze@isitetv.com>
2025-03-03 19:51:54 +0000

    Feat: Add Hero component to home page.

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,5 +1,10 @@
 <script>
 	import Header from '$lib/components/Header/Header.svelte';
+	import Hero from '$lib/components/Hero/Main/Hero.svelte';
+
+	export let data;
 </script>
 
 <Header />
+<Hero movies={data.movies} />
+<Hero movies={data.movies} />
