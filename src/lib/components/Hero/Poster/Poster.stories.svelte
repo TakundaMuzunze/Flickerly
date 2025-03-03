95a18d0da9e9448ff2f65512fcd559b286094491
Takunda <takunda.muzunze@isitetv.com>
2025-03-03 19:48:50 +0000

    Feat: Create movie poster component with poster and text content.

---
+++ src/lib/components/Hero/Poster/Poster.stories.svelte
@@ -0,0 +1,12 @@
+<script module>
+	import { defineMeta } from '@storybook/addon-svelte-csf';
+	import Poster from './Poster.svelte';
+
+	const { Story } = defineMeta({
+		component: Poster,
+		title: 'Components/Movie Poster',
+		tags: ['autodocs']
+	});
+</script>
+
+<Story name="Movie Poster" />
--- src/lib/components/Hero/Poster/Poster.svelte
+++ src/lib/components/Hero/Poster/Poster.svelte
@@ -2,37 +2,47 @@
 	export let selectedMovie;
 </script>
 
-<div class="w-full">
+<div class="flex w-full flex-col items-start justify-start gap-4">
 	{#if selectedMovie}
 		<img
+			class="h-64 self-center md:self-start"
 			src={`https://image.tmdb.org/t/p/w400${selectedMovie.poster_path}`}
 			alt={selectedMovie.title}
 		/>
 
-		<div class="flex flex-col gap-2 py-4">
+		<div class="flex w-full flex-col gap-2 py-4">
 			<h2 class="text-xl">{selectedMovie.title}</h2>
-			<!-- <p class="font-normal">⭐ {selectedMovie.vote_average.toFixed(1)} /10</p> -->
+			<p class="font-normal">⭐ {selectedMovie.vote_average.toFixed(1)} /10</p>
 			<p class="text-base font-normal">{selectedMovie.overview}</p>
 
 			{#if selectedMovie.providers?.GB?.flatrate || selectedMovie.providers?.GB?.rent}
 				<div class="text-accent mt-2 flex flex-col gap-3">
 					<p class="font-semibold">Available to stream or rent on:</p>
-					<div class="flex flex-wrap gap-2">
-						{#each selectedMovie.providers.GB.flatrate || [] as provider}
-							<button class="bg-main-btn rounded-2xl px-3 py-1 text-sm font-normal text-white">
+					<ul class="flex flex-wrap gap-2">
+						{#each selectedMovie.providers.GB.flatrate as provider}
+							<a
+								href={selectedMovie.providers.GB.link || '#'}
+								target="_blank"
+								rel="noopener noreferrer"
+								class="bg-main-btn rounded-2xl px-2 py-1 text-sm text-white"
+							>
 								{provider.provider_name}
-							</button>
+							</a>
 						{/each}
-
 						{#each selectedMovie.providers.GB.rent || [] as provider}
-							<button class="bg-main-btn rounded-2xl px-3 py-1 text-sm font-normal text-white">
-								{provider.provider_name} (Rent)
-							</button>
+							<a
+								href={selectedMovie.providers.GB.link || '#'}
+								target="_blank"
+								rel="noopener noreferrer"
+								class="bg-main-btn rounded-2xl px-3 py-1 text-sm text-white"
+							>
+								{provider.provider_name}
+							</a>
 						{/each}
-					</div>
+					</ul>
 				</div>
 			{:else}
-				<p class="text-sm text-white">Not available for streaming in the UK</p>
+				<p class="text-base text-white">Not available for streaming in the UK</p>
 			{/if}
 		</div>
 	{/if}
