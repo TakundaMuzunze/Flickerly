eb366a10520f34bfae6064a0d26658340ec3ada6
Takunda Muzunze <takmuzunze@gmail.com>
2025-03-23 23:30:20 +0000

    Feat: Create load function to fetch data for details page + add components to route details page.

---
+++ src/routes/movies/[id]/+page.server.ts
@@ -0,0 +1,35 @@
+// src/routes/movies/[id]/+page.server.ts
+import { TMDB_KEY } from '$env/static/private';
+import { error } from '@sveltejs/kit';
+import type { PageServerLoad } from './$types';
+import { fetchProviders } from '$lib/utils/providers';
+
+export const load: PageServerLoad = async ({ params }) => {
+	const { id } = params;
+
+	const movieResponse = await fetch(
+		`https://api.themoviedb.org/3/movie/${id}?api_key=${TMDB_KEY}&language=en-US`
+	);
+
+	const trailerResponse = await fetch(
+		`https://api.themoviedb.org/3/movie/${id}/videos?api_key=${TMDB_KEY}&language=en=US`
+	);
+
+	if (!movieResponse.ok) {
+		throw error(404, 'Movie not found.');
+	}
+
+	if (!trailerResponse.ok) {
+		throw error(404, 'Movie trailer not found.');
+	}
+
+	const movie = await movieResponse.json();
+	const trailerData = await trailerResponse.json();
+	const providers = await fetchProviders(movie.id);
+
+	const trailer =
+		trailerData.results.find((video) => video.type === 'Trailer' && video.site === 'YouTube') ??
+		null;
+
+	return { movie, trailer, providers };
+};
---
+++ src/routes/movies/[id]/+page.svelte
@@ -0,0 +1,32 @@
+<script lang="ts">
+	import DetailsLayout from '$lib/components/Details/DetailsHero/DetailsLayout.svelte';
+	import MovieDetails from '$lib/components/Details/MovieDetails.svelte';
+	import MovieMeta from '$lib/components/Details/MovieMeta/MovieMeta.svelte';
+	import Poster from '$lib/components/Hero/Poster/Poster.svelte';
+	import Trailer from '$lib/components/TrailerComponent/Trailer.svelte';
+
+	export let data;
+
+	// const selectedMovie = data.movie;
+	const trailer = data.trailer;
+
+	const selectedMovie = { ...data.movie, providers: data.providers };
+</script>
+
+<DetailsLayout backgroundImage={selectedMovie.backdrop_path}>
+	<div slot="poster">
+		<Poster {selectedMovie} />
+	</div>
+
+	<div slot="details">
+		<MovieDetails {selectedMovie} />
+	</div>
+
+	<div slot="trailer">
+		<Trailer {trailer} />
+	</div>
+
+	<div slot="overview">
+		<MovieMeta {selectedMovie} />
+	</div>
+</DetailsLayout>
