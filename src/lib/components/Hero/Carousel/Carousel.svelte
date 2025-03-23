30df1434465999c46a43546e48ee6a1c2edce83f
Takunda Muzunze <takmuzunze@gmail.com>
2025-03-23 23:31:13 +0000

    Feat: Amend carousel card so that only active card is clickable to details page.
    Add hover overlay to active card

--- src/lib/components/Hero/Carousel/Carousel.svelte
+++ src/lib/components/Hero/Carousel/Carousel.svelte
@@ -74,16 +74,41 @@
 						<div
 							role="button"
 							tabindex="0"
-							class="embla__slide cursor-pointer transition-transform"
+							class={`embla__slide transition-transform ${
+								i === activeIndex ? 'cursor-pointer opacity-100' : 'pointer-events-none opacity-20'
+							}`}
 							class:selected={i === activeIndex}
-							onclick={() => handleMovieSelect(movie, i)}
-							onkeydown={(e) => (e.key === 'Enter' || e.key === ' ') && handleMovieSelect(movie, i)}
+							onclick={() => i === activeIndex && handleMovieSelect(movie, i)}
+							onkeydown={(e) => {
+								if ((e.key === 'Enter' || e.key === ' ') && i === activeIndex) {
+									handleMovieSelect(movie, i);
+								}
+							}}
 						>
 							<img
 								class="w-full rounded-md shadow-lg"
 								src={`https://image.tmdb.org/t/p/w500${movie.poster_path}`}
 								alt={movie.title || 'Movie poster'}
 							/>
+
+							{#if i === activeIndex}
+								<a
+									aria-label="Link to movie"
+									href={`/movies/${movie.id}`}
+									class="group absolute inset-0 flex w-full items-center justify-center rounded-md bg-black/0 transition-all duration-200 ease-in-out hover:bg-black/60"
+								>
+									<svg
+										class="size-16 opacity-0 transition-all group-hover:opacity-100"
+										xmlns="http://www.w3.org/2000/svg"
+										viewBox="0 0 512 512"
+										fill="white"
+									>
+										<path
+											d="M464 256A208 208 0 1 0 48 256a208 208 0 1 0 416 0zM0 256a256 256 0 1 1 512 0A256 256 0 1 1 0 256zM188.3 147.1c7.6-4.2 16.8-4.1 24.3 .5l144 88c7.1 4.4 11.5 12.1 11.5 20.5s-4.4 16.1-11.5 20.5l-144 88c-7.4 4.5-16.7 4.7-24.3 .5s-12.3-12.2-12.3-20.9l0-176c0-8.7 4.7-16.7 12.3-20.9z"
+										/>
+									</svg>
+								</a>
+							{/if}
 						</div>
 					{/each}
 				</div>
@@ -115,7 +140,6 @@
 		max-width: 280px; /* Prevents them from getting too large on wider screens */
 		position: relative;
 		box-sizing: border-box;
-		opacity: 0.5;
 	}
 
 	@media (min-width: 768px) {
