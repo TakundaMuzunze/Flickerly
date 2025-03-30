22934769d2522f2d7a010d3c483e27095c617741
Takunda Muzunze <takmuzunze@gmail.com>
2025-03-30 17:52:03 +0100

    Refactor: Remove trailer function from load function and import utils function.

--- src/routes/movies/[id]/+page.server.ts
+++ src/routes/movies/[id]/+page.server.ts
@@ -4,6 +4,7 @@ import { error } from '@sveltejs/kit';
 import type { PageServerLoad } from './$types';
 import { fetchProviders } from '$lib/utils/providers';
 import { fetchCast } from '$lib/server/movieCast/+server';
+import { fetchTrailer } from '$lib/utils/trailers';
 
 export const load: PageServerLoad = async ({ params }) => {
 	const { id } = params;
@@ -12,26 +13,14 @@ export const load: PageServerLoad = async ({ params }) => {
 		`https://api.themoviedb.org/3/movie/${id}?api_key=${TMDB_KEY}&language=en-US`
 	);
 
-	const trailerResponse = await fetch(
-		`https://api.themoviedb.org/3/movie/${id}/videos?api_key=${TMDB_KEY}&language=en=US`
-	);
-
 	if (!movieResponse.ok) {
 		throw error(404, 'Movie not found.');
 	}
 
-	if (!trailerResponse.ok) {
-		throw error(404, 'Movie trailer not found.');
-	}
-
 	const movie = await movieResponse.json();
-	const trailerData = await trailerResponse.json();
+	const trailer = await fetchTrailer(movie.id);
 	const providers = await fetchProviders(movie.id);
 	const cast = await fetchCast(movie.id);
-
-	const trailer =
-		trailerData.results.find((video) => video.type === 'Trailer' && video.site === 'YouTube') ??
-		null;
 
 	return { movie, trailer, providers, cast };
 };
