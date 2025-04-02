f91b7c91437c39d8de393be9693412936339f1dd
Takunda Muzunze <takmuzunze@gmail.com>
2025-04-02 21:53:12 +0100

    Feat: Create ne grid component for full movie list pages.

---
+++ src/lib/components/MovieGrid/ResultsGrid.svelte
@@ -0,0 +1,14 @@
+<script lang="ts">
+	import MovieCard from '../MovieCard/MovieCard.svelte';
+
+	export let movies: any[] = [];
+	export let releaseDate: boolean = false;
+</script>
+
+<div class="grid w-full grid-cols-2 gap-6 gap-y-10 sm:grid-cols-3 md:grid-cols-4 xl:grid-cols-6">
+	{#each movies as movie}
+		<div>
+			<MovieCard {movie} showGenre={true} {releaseDate} />
+		</div>
+	{/each}
+</div>
