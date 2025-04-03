9cd00d047c0daef7f15a16816fd58c97f0eb038b
Takunda Muzunze <takmuzunze@gmail.com>
2025-04-03 19:36:18 +0100

    Feat: Amend modal component for release date to be passed through.

--- src/lib/components/Details/MovieDetails.svelte
+++ src/lib/components/Details/MovieDetails.svelte
@@ -1,6 +1,7 @@
 <script lang="ts">
 	import { getDirector } from '$lib/utils/movieCrew';
 	import { getProductionCompany } from '$lib/utils/productionCompanies';
+	import Modal from '../Modal/Modal.svelte';
 	import MovieMeta from './MovieMeta/MovieMeta.svelte';
 
 	export let selectedMovie;
@@ -88,36 +89,13 @@
 	</div>
 </div>
 
-{#if isModalOpen}
-	<div
-		role="button"
-		tabindex="0"
-		class="fixed inset-0 z-50 flex items-center justify-center bg-black/50 backdrop-blur-sm"
-		onclick={closeModal}
-		onkeydown={handleKeydown}
-	>
-		<div
-			class="relative max-h-[80vh] w-full max-w-72 overflow-y-auto rounded-lg bg-gray-900 p-6 shadow-xl md:max-w-2xl"
-		>
-			<button
-				class="absolute top-4 right-4 cursor-pointer text-gray-400 hover:text-white"
-				onclick={closeModal}
-				aria-label="Close modal"
-			>
-				✕
-			</button>
-
-			<div class="mb-4 flex flex-col gap-2">
-				<h2 class="text-2xl font-bold">{selectedMovie.title}</h2>
-				<p class=" text-gray-400">Overview</p>
-			</div>
-			<p class="mb-4 whitespace-pre-wrap text-gray-300">{selectedMovie.overview}</p>
-			<div class="flex flex-col gap-2">
-				<p class="whitespace-pre-wrap text-gray-400">Director: {movieDirector}</p>
-				<p class="whitespace-pre-wrap text-gray-400">
-					Production: {productionCompanies}
-				</p>
-			</div>
-		</div>
-	</div>
-{/if}
+<Modal
+	isOpen={isModalOpen}
+	onClose={() => (isModalOpen = false)}
+	title={selectedMovie.title}
+	subtitle="Overview"
+	content={selectedMovie.overview}
+	releaseDate={selectedMovie.release_date}
+	director={movieDirector}
+	productionCompanies={productionCompanies}
+/>
