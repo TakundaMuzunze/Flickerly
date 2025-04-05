5133a7f9cf89aee6f4531d72d55a743b24be7d8b
Takunda Muzunze <takmuzunze@gmail.com>
2025-04-05 21:12:22 +0100

    Feat: Add consistent page titles for each edge case when search results are made.

--- src/lib/components/Search/SearchResults.svelte
+++ src/lib/components/Search/SearchResults.svelte
@@ -4,19 +4,24 @@
 	export let data;
 </script>
 
-{#if data.error}
-	<div class="text-red-500">
-		{data.error}
-	</div>
-{:else if data.movies.length === 0 && data.query}
-	<div class="text-gray-400">
-		No results found for "{data.query}"
-	</div>
-{:else if data.movies.length > 0}
-	<div class="w-full">
-		<h3 class="mb-6 text-xl font-semibold text-white md:text-2xl">Results for "{data.query}"</h3>
+<div class="w-full">
+	{#if data.error}
+		<h3 class="mb-3 text-xl font-semibold text-white md:mb-6 md:text-2xl">Search Error</h3>
+		<div class="text-red-500">
+			{data.error}
+		</div>
+	{:else if data.movies.length === 0 && data.query}
+		<h3 class="mb-3 text-xl font-semibold text-white md:mb-6 md:text-2xl">No Results Found</h3>
+		<div class="text-gray-400">
+			No movies found for "{data.query}". Try a different search term.
+		</div>
+	{:else if data.movies.length > 0}
+		<h3 class="mb-3 text-xl font-semibold text-white md:mb-6 md:text-2xl">
+			Results for "{data.query}"
+		</h3>
 		<ResultsGrid movies={data.movies} />
-	</div>
-{:else}
-	<div class="text-gray-400">Start typing to search for movies</div>
-{/if}
+	{:else}
+		<h3 class="mb-3 text-xl font-semibold text-white md:mb-6 md:text-2xl">Search Movies</h3>
+		<div class="text-gray-400">Start typing to search for movies</div>
+	{/if}
+</div>
