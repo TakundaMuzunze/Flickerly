85b68d3ccc3a147c8c72e5dd7287c262696153d7
Takunda Muzunze <takmuzunze@gmail.com>
2025-04-05 18:48:15 +0100

    Refactor: Amend search bar component to update results as user is typing query

---
+++ src/lib/components/Search/SearchBar.stories.svelte
@@ -0,0 +1,12 @@
+<script module>
+	import { defineMeta } from '@storybook/addon-svelte-csf';
+	import SearchBar from './SearchBar.svelte';
+
+	const { Story } = defineMeta({
+		component: SearchBar,
+		title: 'Components/Search Bar',
+		tags: ['autodocs']
+	});
+</script>
+
+<Story name="Search Bar Tab" />
