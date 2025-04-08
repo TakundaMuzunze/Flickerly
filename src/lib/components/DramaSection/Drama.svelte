7d5d4e559e3edf37ac11f0fd4d8db416231b8b27
Takunda Muzunze <takmuzunze@gmail.com>
2025-04-08 19:39:32 +0100

    Feat: Create load functions for extra genres to add onto genre homepage/'hub
    Create new section grids using homepage style to add onto genre homepage

---
+++ src/lib/components/DramaSection/Drama.svelte
@@ -0,0 +1,24 @@
+<script lang="ts">
+	import MovieGrid from '../MovieGrid/MovieGrid.svelte';
+	import type { Movie } from '$lib/types/movie';
+	import { fly } from 'svelte/transition';
+
+	export let dramaMovies: Movie[];
+</script>
+
+<section class="mx-auto flex max-w-7xl flex-col items-start justify-start gap-6 p-10">
+	<div in:fly|global={{ y: 50, duration: 650 }} class="flex w-full items-center justify-between">
+		<div class="flex flex-col gap-2">
+			<h2 class="relative text-xl font-semibold text-white md:text-2xl">Drama Movies</h2>
+		</div>
+
+		<a
+			href="/genre/comedy"
+			class="text-accent text-lg font-semibold transition-all duration-300 hover:underline hover:underline-offset-6"
+		>
+			View All →
+		</a>
+	</div>
+
+	<MovieGrid movies={dramaMovies} limit={6} />
+</section>
---
+++ src/routes/genre/+page.svelte
@@ -0,0 +1,16 @@
+<script lang="ts">
+	import Action from '$lib/components/ActionSection/Action.svelte';
+	import Comedy from '$lib/components/ComedySection/Comedy.svelte';
+	import Thriller from '$lib/components/ThrillerSection/Thriller.svelte';
+	import Drama from '$lib/components/DramaSection/Drama.svelte';
+	import Horror from '$lib/components/HorrorSection/Horror.svelte';
+	export let data;
+</script>
+
+<section class="mx-auto flex flex-col items-start justify-start pt-[8rem] xl:pt-[10rem]">
+	<Action actionMovies={data.actionMovies} />
+	<Thriller thrillerMovies={data.thrillerMovies} />
+	<Comedy comedyMovies={data.comedyMovies} />
+	<Drama dramaMovies={data.dramaMovies} />
+	<Horror horrorMovies={data.horrorMovies} />
+</section>
