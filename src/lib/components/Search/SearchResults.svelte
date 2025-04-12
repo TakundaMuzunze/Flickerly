02ea77d10e3df32220dfb0e9cee6a6dcaeb928a7
Takunda Muzunze <takmuzunze@gmail.com>
2025-04-12 23:10:23 +0100

    Feat: Add gap between title and sub-text

--- src/lib/components/Search/SearchResults.svelte
+++ src/lib/components/Search/SearchResults.svelte
@@ -24,8 +24,8 @@
 		<ResultsGrid movies={data.movies} />
 	{:else}
 		<div class="mb-3 md:mb-6">
-			<h3 class=" text-xl font-semibold text-white md:text-2xl">Search Movies</h3>
-			<p class="text-gray-400">Start typing to search for movies</p>
+			<h3 class=" text-xl font-semibold text-white md:text-2xl">Search for movies</h3>
+			<p class="mt-2 text-gray-400">Start typing to search for movies</p>
 		</div>
 	{/if}
 </div>
