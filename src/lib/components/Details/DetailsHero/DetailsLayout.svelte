e9dafd2aabdedc0e10c20b14198a19a1082a8d7b
Takunda Muzunze <takmuzunze@gmail.com>
2025-04-12 23:19:50 +0100

    Style: Reduce gap between elements on details page

--- src/lib/components/Details/DetailsHero/DetailsLayout.svelte
+++ src/lib/components/Details/DetailsHero/DetailsLayout.svelte
@@ -11,7 +11,7 @@
 	<div class="absolute inset-0 bg-black/80"></div>
 
 	<div class="relative z-10 flex w-full max-w-7xl flex-col items-start justify-evenly p-5">
-		<div class="grid w-full grid-cols-1 gap-10 md:grid-cols-12 md:gap-8">
+		<div class="grid w-full grid-cols-1 md:grid-cols-12 md:gap-8">
 			<!-- Poster -->
 			<div class="md:col-span-4 xl:col-span-3">
 				<slot name="poster" />
