5f8630e8ad5a7d8e0a701e6a345ffb35881e3081
Takunda Muzunze <takmuzunze@gmail.com>
2025-04-12 23:31:26 +0100

    Feat: Remove view all link for cast members

--- src/lib/components/Details/Cast/CastGrid.svelte
+++ src/lib/components/Details/Cast/CastGrid.svelte
@@ -7,12 +7,6 @@
 <div class="flex flex-col gap-10">
 	<div class="flex w-full items-center justify-between">
 		<h3 class="text-2xl font-semibold text-white">Cast:</h3>
-		<a
-			href="/trending"
-			class="hover:text-accent text-lg font-semibold text-white transition-all duration-200 ease-in-out"
-		>
-			View All →
-		</a>
 	</div>
 	<div
 		class="flex w-full gap-4 overflow-x-auto scroll-smooth whitespace-nowrap xl:grid xl:grid-cols-3"
