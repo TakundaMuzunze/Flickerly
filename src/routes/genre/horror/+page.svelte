9801b08418dc70058b379219d9781a3fee58b61d
Takunda Muzunze <takmuzunze@gmail.com>
2025-04-15 23:24:31 +0100

    Style: Fix typography errors

--- src/routes/genre/horror/+page.svelte
+++ src/routes/genre/horror/+page.svelte
@@ -8,7 +8,7 @@
 	class="mx-auto flex max-w-7xl flex-col items-start justify-start gap-8 p-5 pt-[8rem] xl:p-10 xl:pt-[10rem]"
 >
 	<div class="space-y-2">
-		<h2 class="relative text-xl font-semibold text-white md:text-2xl">Horror movies</h2>
+		<h2 class="relative text-xl font-semibold text-white md:text-2xl">Horror Movies</h2>
 		<p class="text-gray-300 md:text-lg">{genreSubtitles.horror}</p>
 	</div>
 	<ResultsGrid movies={data.horrorMovies} />
--- src/routes/genre/science-fiction/+page.svelte
+++ src/routes/genre/science-fiction/+page.svelte
@@ -8,7 +8,7 @@
 	class="mx-auto flex max-w-7xl flex-col items-start justify-start gap-8 p-5 pt-[8rem] xl:p-10 xl:pt-[10rem]"
 >
 	<div class="space-y-2">
-		<h2 class="relative text-xl font-semibold text-white md:text-2xl">Science Fiction movies</h2>
+		<h2 class="relative text-xl font-semibold text-white md:text-2xl">Science Fiction Movies</h2>
 		<p class="text-gray-300 md:text-lg">{genreSubtitles.scifi}</p>
 	</div>
 	<ResultsGrid movies={data.scifiMovies} />
