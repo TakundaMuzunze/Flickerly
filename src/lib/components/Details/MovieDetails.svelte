1095827fec5e58a7774f2be032b5c40580a59975
Takunda Muzunze <takmuzunze@gmail.com>
2025-04-18 12:52:00 +0100

    Feat: Create button to add movie to watch list

--- src/lib/components/Details/MovieDetails.svelte
+++ src/lib/components/Details/MovieDetails.svelte
@@ -89,6 +89,23 @@
 					</g></svg
 				>
 			</button>
+
+			<button
+				aria-label="Open movie information modal"
+				class="group flex aspect-square size-11 cursor-pointer items-center justify-center rounded-full bg-white/25 backdrop-blur-lg transition-all duration-300 ease-in-out hover:scale-105 hover:bg-white"
+				onclick={() => (isModalOpen = true)}
+			>
+				<svg
+					class="size-5 transition-colors duration-200 group-hover:fill-black md:size-6"
+					fill="white"
+					xmlns="http://www.w3.org/2000/svg"
+					viewBox="0 0 448 512"
+					><path
+						class="group-hover:stroke-black"
+						d="M256 80c0-17.7-14.3-32-32-32s-32 14.3-32 32l0 144L48 224c-17.7 0-32 14.3-32 32s14.3 32 32 32l144 0 0 144c0 17.7 14.3 32 32 32s32-14.3 32-32l0-144 144 0c17.7 0 32-14.3 32-32s-14.3-32-32-32l-144 0 0-144z"
+					/></svg
+				>
+			</button>
 		</div>
 
 		{#if selectedMovie.providers?.GB?.flatrate || selectedMovie.providers?.GB?.rent}
