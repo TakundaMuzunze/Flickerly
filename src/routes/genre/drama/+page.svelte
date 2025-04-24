7e2fde203aec9a4c558fc5f90b676859a46ba48e
Takunda Muzunze <takmuzunze@gmail.com>
2025-04-24 23:15:48 +0100

    feat: Adjust grid widths on each genre full page

--- src/routes/genre/drama/+page.svelte
+++ src/routes/genre/drama/+page.svelte
@@ -19,7 +19,7 @@
 </script>
 
 <section
-	class=" min-w-screen-2xl flex flex-col items-start justify-start gap-8 p-5 pt-[8rem] xl:p-10 xl:pt-[10rem]"
+	class="min-w-screen-2xl flex flex-col items-start justify-start gap-8 p-5 pt-[8rem] xl:p-10 xl:pt-[10rem]"
 >
 	<div class="space-y-2">
 		<h2 class="relative text-xl font-semibold text-white md:text-2xl">Drama Movies</h2>
--- src/routes/trending/+page.svelte
+++ src/routes/trending/+page.svelte
@@ -6,7 +6,7 @@
 </script>
 
 <section
-	class="mx-auto flex max-w-7xl flex-col items-start justify-start gap-8 p-5 pt-[8rem] xl:p-10 xl:pt-[10rem]"
+	class="min-w-screen-2xl flex flex-col items-start justify-start gap-8 p-5 pt-[8rem] xl:p-10 xl:pt-[10rem]"
 >
 	<div class="space-y-2">
 		<h2 class="relative text-xl font-semibold text-white md:text-2xl">Trending Movies</h2>
