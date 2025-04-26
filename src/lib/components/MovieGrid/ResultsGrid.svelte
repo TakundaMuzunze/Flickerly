8a971d870f2f8eb534cca1674a00b0bc96ae39bb
Takunda Muzunze <takmuzunze@gmail.com>
2025-04-26 18:08:16 +0100

    fix: resolve issue with search results not appearing
    amend search results grid to match layout with the rest of application

--- src/lib/components/MovieGrid/ResultsGrid.svelte
+++ src/lib/components/MovieGrid/ResultsGrid.svelte
@@ -1,5 +1,4 @@
 <script lang="ts">
-	import { movieStore } from '$lib/stores/movieStore.stores.svelte';
 	import MovieCard from '../MovieCard/MovieCard.svelte';
 	import { fade } from 'svelte/transition';
 
@@ -9,7 +8,7 @@
 <div
 	class="grid w-full grid-cols-2 gap-y-8 sm:grid-cols-3 md:grid-cols-4 xl:grid-cols-6 2xl:grid-cols-8"
 >
-	{#each $movieStore.movies as movie (movie.id)}
+	{#each movies as movie (movie.id)}
 		<div in:fade={{ duration: 200 }}>
 			<MovieCard {movie} showGenre={true} {releaseDate} />
 		</div>
