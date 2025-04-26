adc2731e8d482cb06c7eb1a249c1b0f1d2b24c32
Takunda Muzunze <takmuzunze@gmail.com>
2025-04-26 11:59:23 +0100

    feat: create grid to store watchlist movies

---
+++ src/lib/components/MovieGrid/WatchlistGrid.svelte
@@ -0,0 +1,16 @@
+<script lang="ts">
+	import MovieCard from '../MovieCard/MovieCard.svelte';
+	import { fade } from 'svelte/transition';
+
+	const { movies = [], releaseDate = false } = $props();
+</script>
+
+<div
+	class="grid w-full grid-cols-2 gap-y-8 sm:grid-cols-3 md:grid-cols-4 xl:grid-cols-6 2xl:grid-cols-8"
+>
+	{#each movies as movie (movie.id)}
+		<div in:fade={{ duration: 200 }}>
+			<MovieCard {movie} showGenre={true} {releaseDate} />
+		</div>
+	{/each}
+</div>
