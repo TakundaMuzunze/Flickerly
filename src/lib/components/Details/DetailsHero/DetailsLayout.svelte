94c56328e35e35c8e57798664459ca6ea836690a
Takunda Muzunze <takmuzunze@gmail.com>
2025-04-27 17:01:10 +0100

    style: change section height to screen and aligned text at the bottom of container

--- src/lib/components/Details/DetailsHero/DetailsLayout.svelte
+++ src/lib/components/Details/DetailsHero/DetailsLayout.svelte
@@ -3,12 +3,12 @@
 </script>
 
 <section
-	class="min-w-screen-2xl relative flex min-h-[75vh] w-full flex-col items-start justify-center bg-cover bg-top py-5 pt-[5rem] text-white md:px-8"
+	class="min-w-screen-2xl relative flex min-h-screen w-full flex-col items-start justify-end bg-cover bg-top py-5 pt-[5rem] text-white md:px-8"
 	style="background-image: {backgroundImage
 		? `url(https://image.tmdb.org/t/p/original${backgroundImage})`
 		: 'none'}"
 >
-	<div class="absolute inset-0 bg-black/70"></div>
+	<div class="absolute inset-0 bg-black/40"></div>
 
 	<div class="relative z-10 flex w-full max-w-7xl flex-col items-start justify-evenly p-5">
 		<div class="grid w-full grid-cols-1 md:grid-cols-12 md:gap-8">
