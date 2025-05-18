285a367a3c0a93daf5fad13b6785d035d6460f19
Takunda Muzunze <takmuzunze@gmail.com>
2025-05-18 19:13:37 +0100

    perf: amend sizing of cast cards and use different dimensions from tmdb to reduce image size

--- src/lib/components/Details/Cast/CastCard.svelte
+++ src/lib/components/Details/Cast/CastCard.svelte
@@ -2,17 +2,18 @@
 	export let actor;
 </script>
 
-<div class="flex max-w-36 flex-col items-start rounded-full lg:max-w-48">
+<div class="flex flex-col items-start rounded-full">
 	<img
 		src={actor.profile_path
 			? `https://image.tmdb.org/t/p/w342/${actor.profile_path}`
 			: '/Frame.png'}
 		alt="Actor headshot"
-		class="aspect-square rounded-full object-cover object-center"
+		class="aspect-square max-w-28 rounded-full object-cover object-center
+ md:max-w-40 xl:max-w-48"
 	/>
 	<div class="mt-3 w-full">
 		<div class="flex flex-col items-center justify-center gap-1 text-center text-wrap">
-			<p class="font-semibold text-white lg:text-lg">{actor.name}</p>
+			<p class="font-semibold text-white max-md:text-sm lg:text-lg">{actor.name}</p>
 			{#if actor.character}
 				<p class=" text-sm text-gray-400">as {actor.character}</p>
 			{/if}
