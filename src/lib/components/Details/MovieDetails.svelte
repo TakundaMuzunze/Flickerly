214d604f9fee9fadcc75b4073b9587c427ed847b
Takunda Muzunze <takmuzunze@gmail.com>
2025-05-18 16:12:32 +0100

    feat: create another modal for sharing links with functionality to share on the 3 different platforms

--- src/lib/components/Details/MovieDetails.svelte
+++ src/lib/components/Details/MovieDetails.svelte
@@ -8,11 +8,14 @@
 	import InfoButton from './Buttons/InfoButton.svelte';
 	import WatchlistButton from './Buttons/WatchlistButton.svelte';
 	import StreamingProviders from './StreamingProviders/StreamingProviders.svelte';
+	import SocialModal from '../Modal/SocialModal.svelte';
+	import ShareButton from './Buttons/ShareButton.svelte';
 
 	export let selectedMovie;
 	export let trailer;
 
 	let isModalOpen = false;
+	let isSocialModalOpen = false;
 	let isTrailerModalOpen = false;
 
 	$: movieDirector = getDirector(selectedMovie.credits?.crew);
@@ -40,6 +43,7 @@
 			<TrailerButton onClick={() => (isTrailerModalOpen = true)} />
 			<InfoButton onClick={() => (isModalOpen = true)} />
 			<WatchlistButton movie={selectedMovie} />
+			<ShareButton onClick={() => (isSocialModalOpen = true)} />
 		</div>
 
 		<StreamingProviders providers={selectedMovie.providers} movieTitle={selectedMovie.title} />
@@ -55,6 +59,14 @@
 	director={movieDirector}
 	{productionCompanies}
 	{certification}
+/>
+
+<SocialModal
+	isOpen={isSocialModalOpen}
+	content={selectedMovie.overview}
+	movieTitle={selectedMovie.title}
+	movieId={selectedMovie.id}
+	onClose={() => (isSocialModalOpen = false)}
 />
 
 <Trailer {trailer} isOpen={isTrailerModalOpen} onClose={() => (isTrailerModalOpen = false)} />
